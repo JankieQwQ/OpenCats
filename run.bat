6081dbe0180eba1bf2cde87f9c7279ae2a8850c2
Soren2024 <jankie2023@outlook.com>
2024-02-15 20:19:33 +0800

    Change tips

--- run.bat
+++ run.bat
@@ -3,7 +3,7 @@ title OpenCats V2
 cd src
 python check.py
 start python init.py
-echo Web面板已经在 http://127.0.0.1:9000/ 启动。
+echo HTTP Server run on http://127.0.0.1:9000/.
 python -m http.server 9000 --directory ./web/
 echo 程序已退出。
 pause
