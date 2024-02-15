79f17b82f38f52751416fdd5711b2ecc2e07f06d
Soren2024 <jankie2023@outlook.com>
2024-02-15 20:18:57 +0800

    File open test

---
+++ check.py
@@ -0,0 +1,16 @@
+def openTest(filename:bool) -> bool:
+    try:
+        file = open(filename)
+        file.close()
+        return True
+    except:
+        return False
+
+def canOpen(filename:str,isOK:bool) -> None:
+    print('[{}] OpenCats File Check:{}'.format(str(isOK).replace('True','OK').replace('False','ERROR'),filename))
+    return
+
+filelist = ['./src/init.py','./src/server.py','./src/web/index.html','./check.py','./run.bat','./other/ChatGPT_DAN.tex','./other/latexboom.tex','./other/markdownoom.md','./other/usbot.py']
+
+for name in filelist:
+    canOpen(openTest(name))
--- run.bat
+++ run.bat
@@ -1,6 +1,7 @@
 @echo off
 title OpenCats V2
 cd src
+python check.py
 start python init.py
 echo Web面板已经在 http://127.0.0.1:9000/ 启动。
 python -m http.server 9000 --directory ./web/
